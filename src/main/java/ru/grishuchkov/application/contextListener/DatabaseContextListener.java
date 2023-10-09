19d619e0e7d0b6237a26a3fe9ea3f5d3ef15da64
grishuchkov <gri.d.a@ya.ru>
2023-10-09 11:35:52 +0300

    delete: DatabaseConnectionManager

--- src/main/java/ru/grishuchkov/application/contextListener/DatabaseContextListener.java
+++ src/main/java/ru/grishuchkov/application/contextListener/DatabaseContextListener.java
@@ -1,19 +1,9 @@
 package ru.grishuchkov.application.contextListener;
 
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
-import ru.grishuchkov.application.DatabaseConnectionManager;
 
 @WebListener(value = "1")
 public class DatabaseContextListener implements ServletContextListener {
-
-    @Override
-    public void contextInitialized(ServletContextEvent sce) {
-        ServletContext servletContext = sce.getServletContext();
-        DatabaseConnectionManager connectionManager = new DatabaseConnectionManager();
-        servletContext.setAttribute("connectionManager", connectionManager);
-    }
 
 }
