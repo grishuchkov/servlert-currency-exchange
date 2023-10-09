aadb067bdd86a7ecdba829a25a5094239f70b112
grishuchkov <gri.d.a@ya.ru>
2023-10-09 11:31:57 +0300

    add: save new currency

--- src/main/java/ru/grishuchkov/application/servlet/CurrenciesServlet.java
+++ src/main/java/ru/grishuchkov/application/servlet/CurrenciesServlet.java
@@ -10,6 +10,7 @@ import ru.grishuchkov.application.exception.handler.AppExceptionHandler;
 import ru.grishuchkov.application.service.CurrencySerivceImpl;
 import ru.grishuchkov.application.service.ifcs.CurrencyService;
 import ru.grishuchkov.application.utils.JsonResponse;
+import ru.grishuchkov.application.utils.Validator;
 
 import java.io.IOException;
 
@@ -24,6 +25,20 @@ public class CurrenciesServlet extends HttpServlet {
             JsonResponse.send(resp, currencyService.getAllCurrencies(), HttpServletResponse.SC_OK);
         } catch (AppException exception) {
             AppExceptionHandler.handle(resp, exception);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String code = req.getParameter("code").toUpperCase();
+        String sign = req.getParameter("sign").toUpperCase();
+
+        try {
+            Validator.validateCurrency(name, code, sign);
+            JsonResponse.send(resp, currencyService.add(name, code, sign), HttpServletResponse.SC_OK);
+        } catch (AppException e) {
+            AppExceptionHandler.handle(resp, e);
         }
     }
 }
