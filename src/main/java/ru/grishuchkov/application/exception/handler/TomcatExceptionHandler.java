31b66d9511803f34b44c9f362eefb447f2a403a0
grishuchkov <gri.d.a@ya.ru>
2023-10-11 10:46:54 +0300

    refactor: add exception message

--- src/main/java/ru/grishuchkov/application/exception/handler/TomcatExceptionHandler.java
+++ src/main/java/ru/grishuchkov/application/exception/handler/TomcatExceptionHandler.java
@@ -12,6 +12,20 @@ import java.io.IOException;
 
 @WebServlet(name = "errorHandlerServlet", urlPatterns = {"/errorHandler"}, loadOnStartup = 1)
 public class TomcatExceptionHandler extends HttpServlet {
+
+    private void processError(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        Throwable exception = (Throwable) request.getAttribute("jakarta.servlet.error.exception");
+        String errorMessage = (String) request.getAttribute("jakarta.servlet.error.message");
+        Integer statusCode = (Integer) request.getAttribute("jakarta.servlet.error.status_code");
+
+        if (exception != null){
+            errorMessage = exception.getCause().toString();
+        }
+
+        JsonResponse.send(response, new ExceptionDto(errorMessage, statusCode), statusCode);
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         processError(req, resp);
@@ -35,13 +49,5 @@ public class TomcatExceptionHandler extends HttpServlet {
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         processError(req, resp);
-    }
-
-    private void processError(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-        String errorMessage = (String) request.getAttribute("jakarta.servlet.error.message");
-        Integer statusCode = (Integer) request.getAttribute("jakarta.servlet.error.status_code");
-
-        JsonResponse.send(response, new ExceptionDto(errorMessage, statusCode), statusCode);
     }
 }
