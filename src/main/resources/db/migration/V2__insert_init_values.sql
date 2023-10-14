fcccc3b712876c626291fa425c13d75c07f1e905
grishuchkov <gri.d.a@ya.ru>
2023-10-14 19:37:47 +0300

    fix: rates and bug with bad id's

--- src/main/resources/db/migration/V2__insert_init_values.sql
+++ src/main/resources/db/migration/V2__insert_init_values.sql
@@ -4,9 +4,12 @@ VALUES (1, 'USD', 'United States dollar', '$'),
        (3, 'RUB', 'Russian Ruble', '₽'),
        (4, 'AUD', 'Australian dollars', 'A$'),
        (5, 'CNY', 'Yuan Renminbi', '¥');
+ALTER SEQUENCE currencies_id_seq RESTART WITH 6;
 
 
-INSERT INTO exchange_rates(base_currency_id, target_currency_id, rate)
-VALUES (1, 2, 0.99),
-       (1, 3, 100),
-       (3, 5, 12);
+INSERT INTO exchange_rates(id, base_currency_id, target_currency_id, rate)
+VALUES (1, 1, 2, 0.941377),
+       (2, 1, 3, 96.990000),
+       (3, 1, 4, 1.560000),
+       (4, 3, 5, 0.075555);
+ALTER SEQUENCE exchange_rates_id_seq RESTART WITH 5;
